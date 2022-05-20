694033b0aa586a02d1dc7e0db212bf7ff6d6c03f
bsorinnn <bxxsxrxx@gmail.com>
2022-05-20 19:22:47 +0900

    [FEAT] 향유 연습용 프로젝트 초기 생성 (#1)
    - 폴더링
    - Podfile 설치
    - Code Base로 설정

---
+++ HyangyuPractice/HyangyuPractice/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HyangyuPractice
+//
+//  Created by 배소린 on 2022/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
